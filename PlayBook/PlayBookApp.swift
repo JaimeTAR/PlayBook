49952d3955fc16fcbf88da5e6c500eb932a45751
Jaime Alfaro <jaimetadeoalfaro@hotmail.com>
2024-03-30 17:13:09 -0600

    Initial Commit

---
+++ PlayBook/PlayBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayBookApp.swift
+//  PlayBook
+//
+//  Created by Jaime Tadeo Alfaro Rodríguez on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
